14b3a10d9d76ced9c8e31255124c8f1df0234360
Luis Almanzar <ljalmanzar@nevada.unr.edu>
2015-09-08 13:37:05 -0700

    Practicing

---
+++ CS480/PA1/src/loader.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+string shaderLoader(const char* fileName)
+{
+	// open file
+	ifstream fin(fileName);
+
+	// create stream object
+	stringstream sStream;
+
+	// read in the entire file
+	sStream << fin.rdbuf();
+
+	// save the string for return
+	string str = sStream.str();
+
+	cout << str << endl;
+
+	return str;
+}
