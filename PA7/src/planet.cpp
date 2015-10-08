c5886fe79a4b22a2aa6da2d4096ab14d410b99ed
Gunnar <gwambaugh@nevada.unr.edu>
2015-10-08 12:15:25 -0700

    updated planet class, updated example 2 file

--- PA7/src/planet.cpp
+++ PA7/src/planet.cpp
@@ -19,35 +19,81 @@ Boolean initialize (const std::string &fileName){
 	ifstream fin;
 	fin.open( fileName.c_str());
 	if (!fin.good()){
-		stid::cerr << "Cannot find file: "
+		stid::cerr << "ERROR: Cannot find file: "
 				   << fileName << std::endl;
 		return false;
 	}
-
-	while (fin.good()){
-		std::string target;
-		fin << target;
 		
-		if (target.compare("model") == 0){
-			fin << target;
-		} else if (target.compare("planet") == 0){
-			fin << target;
-		} else if (target.compare("dir") == 0){
-			fin << target;
-		} else if (target.compare("rotationSpd") == 0){
-			fin << target;
-		} else if (target.compare("orbitRad") == 0){
-			fin << target;
-		} else if (target.compare("orbitSpd") == 0){
-			fin << target;
-		} else if (target.compare("#") == 0){
-			
-		} else{
-			std::cerr << "Unkown keyword: " 
-				      << target << std::endl;
+	std::string line;
+	
+	while (fin.good()){
+		
+		std::getline(fin, line)
+		
+		// get object file name
+		if (line.substr(0,5) == "model "){ //string
+			const std::string temp = line.substr(5);
+			std::sscanf(temp, "%s", _objectFile);
+		} 
+		// get point of origin
+		else if (line.substr(0,6) == "planet "){ //string
+			const std::string temp = line.substr(6);
+			std::sscanf(temp, "%s", _targetKey);
+		} 
+		// get point of origin 
+		else if (line.substr(0,9) == "crotation "){
+			const std::string temp = line.substr(9);
+			std::sscanf(temp, "%f %f %f"
+						, &_centerOfRotation.x
+						, &_centerOfRotation.y
+						, &_centerOfRotation.z)
+		}
+		// get the direction scalar
+		else if (line.substr(0,3) == "dir "){ // int
+			const std::string temp = line.substr(3);
+			std::sscanf(temp, "%i", _rotationDirection);
+		} 
+		// get the speed of rotation
+		else if (line.substr(0,6) == "rspeed "){ // float
+			const std::string temp = line.substr(6);
+			std::sscanf(temp,"%f", _rotationSpeed);
+		}
+		// get the distance of orbit 
+		else if (line.substr(0,7) == "oradius "){ // float
+			const std::string temp = line.substr(7);
+			std::scanf(temp, "%f", _orbitRadius);
+		} 
+		// get the speed/rate of orbit
+		else if (line.substr(0,6) == "ospeed "){ // float
+			const std::string temp = line.substr(6);
+			std::scanf(temp, "%f", _orbitSpeed);
+		} 		
+		// get planet radius
+		else if (line.substr(0,6) == "pradius "){ // float
+			const std::string temp = line.substr(6);
+			std::scanf(temp, "%f", _planetRadius);
+		} 
+		// ignore for comments
+		else if (line.substr(0,1) == "# "){
+			// comment, so skip line aand do nothing
+		} 
+		// error if unknown command
+		else{
+			// not a proper format.
+			std::cerr << "ERROR: Unkown keyword in line: " 
+				      << line << std::endl;
 				      return false;
 		}
+			}
 	}
+	// exit file
+	fin.close();
+
+	return true;
+}
+
+std::string Planet::getTargetKey(){
+	return _targetKey;
 }
 
 Vertex Planet::getVertex() const {
--- PA7/src/planet.h
+++ PA7/src/planet.h
@@ -4,23 +4,42 @@
 
 class Planet{
 	public:
-		Planet();
-		Planet(const std::String &fileName);
-		~Planet();
-		Boolean initialize(const std::string &fileName);
 
-		Vertex getVertex() const;
+		// constructors/destructors
+		Planet();
+		Planet(const std::String &fileName, SolarSysyem);
+		~Planet();
+
+		// operators
+		void update(float dt, const &Planet);
+
+		// setters
+		Boolean initialize(const std::string &fileName);
+		void setGeometry();
+
+		// getters
+		Vertex getGeometry() const;
 		glm::mat4 getModel() const;
+		std::string getTargetKey() const;
 		
 	private:
-		Vertex _vertex;
+
+		void _setCenterOfRotation();
+
+		// variables
+		Vertex _geometry; 
 		glm::mat4 _model;
-		std::string &_objectFile;
-		float _centerOfOrbit;
+
+		Planet _target;
 		glm::vec3 _centerOfRotation;
+		
+		std:string &_targetKey;
+		std::string &_objectFile;
+
 		float _rotationSpeed;
 		float _orbitRadius;
 		float _orbitSpeed;
-		int _rotationDirection;
 		float _planetRadius;
+
+		int _rotationDirection;
 };
