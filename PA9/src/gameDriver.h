b35ab9063f7c644e192c62a1de6a046cf2a11ce3
Gunnar <gwambaugh@nevada.unr.edu>
2015-10-23 11:53:21 -0700

    added baseline for gameDriver class

--- PA9/src/gameDriver.h
+++ PA9/src/gameDriver.h
@@ -1,6 +1,26 @@
+#include "player.cpp"
+#include "GLD.cpp"
+#include "powerup.cpp"
+#include <iostream>
+
 class GameDriver{
 	public:
 		GameDriver();
 		~GameDriver();
+
+		void initGame();
+
+		// setters
+
+		// getters
+		Player getPlayer1()const;
+		Player getPlayer2()const; 
+		std::vector<GLD> getPucks()const;
+
 	private:
+		Player _player1;
+		Player _player2;
+
+		GLD _table;
+		std::vector<GLD> _pucks;
 };
--- PA9/src/powerup.h
+++ PA9/src/powerup.h
@@ -6,4 +6,5 @@ class Powerup{
 		Powerup();
 		~Powerup();
 	private:
+		
 };
