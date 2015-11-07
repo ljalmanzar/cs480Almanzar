35921588ca65b232e31a2acd9ee30ca6834b1d35
Luis Almanzar <ljalmanzar@gmail.com>
2015-11-06 21:56:21 -0800

    cylinders

--- PA9/src/player.cpp
+++ PA9/src/player.cpp
@@ -28,7 +28,7 @@ void Player::init(){
 	else{
 		_paddle.initialize("../bin/paddle_blue.obj"
 						  	,"../bin/blue_while_red.png",
-						  		true, SPHERE);
+						  		true, CYLINDER);
 		tempModel = glm::translate( 
         	_paddle.getModel(),
         	glm::vec3(-10.0f, 6.0f, 0.0f) 
